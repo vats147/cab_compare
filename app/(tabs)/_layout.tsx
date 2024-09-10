e2579e0a6de2fdaea3d4afbf8b657d7a2bf9b79e
vats <vatsalmangukiya9003@gmail.com>
2024-09-10 17:59:07 +0530

    inital commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,8 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
+import { Image, StyleSheet } from 'react-native';
+import { Tabs } from 'expo-router'; // Make sure you're using the right `Tabs` component import
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -13,25 +12,47 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          title: 'Ola',
+          tabBarIcon: ({ focused }: { focused: boolean }) => (
+            <Image
+              source={
+                focused
+                  ? require('@/assets/images/ola-icon.png')
+                  : require('@/assets/images/ola-icon.png')
+              }
+              style={styles.icon}
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          title: 'Uber',
+          tabBarIcon: ({ focused }: { focused: boolean }) => (
+            <Image
+              source={
+                focused
+                  ? require('@/assets/images/uber-logo.png')
+                  : require('@/assets/images/uber-logo.png')
+              }
+              style={styles.icon}
+            />
           ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 32,
+    height: 32,
+  },
+});
